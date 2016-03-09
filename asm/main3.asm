8a8e4452dbe644eb07978edc42628bd01aeaf0d7
Noah Huetter <noahhuetter@gmail.com>
2016-03-09 11:51:46 +0100

    added exercise 3

--- asm/main3.asm
+++ asm/main3.asm
@@ -10,43 +10,61 @@ init:
 	; PortC output LEDs
     ldi r16, 0xff
 	out DDRC, r16
-	ldi r16, 0b00000011
+	ldi r16, 0b00000000
 	out PORTC, r16
 	; PortA 2 low bits output
 	ldi r16, 0b00000011
 	out DDRA, r16
 	ldi r16, 0b00000001
 	out PORTA, r16
+	; PortA 2,3 input pull enable
+	sbi PORTA, 2
+	sbi PORTA, 3
+
 	; init stack pointer
 	ldi r16, high(RAMEND)
 	out SPH, r16
 	ldi r16, low(RAMEND)
 	out SPL, r16
 
+	ldi r30, 0x00
+
+
+main:
+	call wait
+	in r16, PINA
+	ror r16
+	ror r16
+	ldi r17, 0x01
+	and r16, r17
+	; now the button is on the first bit
+	ldi r17, 0x00
+	cpse r16, r17
+	rjmp main
+	; if register is 0, continue here
+	inc r30
+	out PORTC, r30
+	rjmp main
+
+
+
 
 ; Replace with your application code
-left:
-	in r16, PORTC
-	rol r16
-	out PORTC, r16
-	call wait
-	ldi r30, 0b11000000
-	cpse r16, r30
-	rjmp left
-
-right:
-	in r16, PORTC
+test:
+	in r16, PINA
 	ror r16
-	out PORTC, r16
-	call wait
-	ldi r30, 0b00000011
-	cpse r16, r30
-	rjmp right
-	rjmp left
+	ror r16
+	ldi r17, 0b00000011
+	and r16, r17
+	ldi r17, 0b00001100
+	or r16, r17
+	out PORTA, r16
+	rjmp test
+	
 
 
 wait:
-	ldi r20, 2
+	ldi r20, 20
 	ldi r21, 200
 	ldi r22, 200
 loop1:
